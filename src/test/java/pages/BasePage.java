c665f5347bfef6a704ccea481cd55608bb59b133
Vinhhoang-1312 <tranphanvinhhoang7@gmail.com>
2025-09-30 15:33:09 +0700

    fix conflict

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -11,7 +11,7 @@ public class BasePage {
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15)); // Increased timeout
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         PageFactory.initElements(driver, this);
     }
 
@@ -21,5 +21,12 @@ public class BasePage {
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
+    }
+    public String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
+
+    public String getPageTitle() {
+        return driver.getTitle();
     }
 }
