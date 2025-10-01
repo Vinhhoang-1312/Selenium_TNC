b7fc9aac422bd134cf1fec50b492110a7066fcb4
Dung <andrew.td2003@gmail.com>
2025-10-01 13:34:00 +0700

    Revert "Merge pull request #14 from gitdung/Test/Search_ProductDetail"
    This reverts commit 4333e9770c820758731febccbae86c53a0194d7e, reversing
    changes made to 9b98469e78317f225d5f9639ca57666a7c334fd6.

--- open-test-report.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Opening TNC Store Test Report...
-echo.
-echo Main Report File: target\TNC_Store_Test_Report.html
-echo.
-start "" "target\TNC_Store_Test_Report.html"
-echo Report opened in your default browser!
-pause
-
