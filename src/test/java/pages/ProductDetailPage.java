2b511788fd639212808e02d46f60d65effaea549
Dung <andrew.td2003@gmail.com>
2025-10-02 08:50:42 +0700

    #refactor
    fix all errors

--- src/test/java/pages/ProductDetailPage.java
+++ src/test/java/pages/ProductDetailPage.java
@@ -18,6 +18,7 @@ public class ProductDetailPage extends BasePage {
     public static By itemnameInMainPage = org.openqa.selenium.By.xpath("//div[@id=\"js-product-cate-79\"]//div[@class=\"owl-item active\"][2]//a[contains(@class,'product-name')]");
     public static By itemInMainPage = org.openqa.selenium.By.xpath("//div[contains(@id,'79')]//div[@class='owl-item active'][2]");
     public static By productName = org.openqa.selenium.By.xpath("//h1[@class='name']");
+    public static By successNotification = By.xpath("//div[@class='content-container']");
     public static By originPrice = org.openqa.selenium.By.xpath("//div[@class='info-main-price']//del[@class='old-price']");
     public static By Price = org.openqa.selenium.By.xpath("//div[@class='info-main-price']//div[@class='price']");
     public static By SaleOff = org.openqa.selenium.By.xpath("//div[@class='info-main-price']//div[@class='saleoff']");
