6f8012a580a51b541099ff8fa545b14e9b777b4c
votanquocc <quoc1610@outlook.com>
2025-10-05 09:58:33 +0700

    make driver_factory can use all driver but prioritize edge

--- src/main/java/commons/Driver_Factory.java
+++ src/main/java/commons/Driver_Factory.java
@@ -1,25 +1,40 @@
 package commons;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Driver_Factory {
     private static WebDriver driver;
-    
-
-        public static WebDriver getDriver() {
-            if (driver == null) {
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            try {
                 driver = new EdgeDriver();
-                driver.manage().window().maximize();
+            } catch (Exception e1) {
+                System.out.println("Không thể khởi tạo EdgeDriver, thử ChromeDriver...");
+                try {
+                    driver = new ChromeDriver();
+                } catch (Exception e2) {
+                    System.out.println("Không thể khởi tạo ChromeDriver, thử FirefoxDriver...");
+                    try {
+                        driver = new FirefoxDriver();
+                    } catch (Exception e3) {
+                        System.out.println("Không thể khởi tạo bất kỳ trình điều khiển nào.");
+                        throw new RuntimeException("Không thể khởi tạo WebDriver", e3);
+                    }
+                }
             }
-            return driver;
+            driver.manage().window().maximize();
         }
-        public static void quitDriver() {
-            if (driver != null) {
-                driver.quit();
-                driver = null;
-            }
+        return driver;
+    }
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
         }
+    }
 
 
 }
